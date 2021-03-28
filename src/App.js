8b8b8ad127877bc51a138bc9e8f1708577bf853e
codejunkie1609 <sricharan1609@gmail.com>
2021-03-28 15:12:32 +0530

    completed login and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 function App() {
   return (
+    <div className="App" >
+      <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+
+        </div>
+      </div>
+    </div></Router>
+      
     </div>
   );
 }
---
+++ src/components/signup.component.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                <h3>Sign Up</h3>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input id="email" type="email" className="form-control" placeholder="Enter email" />
+                </div>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input id="username" type="text" className="form-control" placeholder="Enter Username" />
+                </div>
+
+                <div className="form-group">
+                    <label>Mobile Number</label>
+                    <input type="text" id="mobilenumber" className="form-control" placeholder="Enter Mobile Number" />
+                </div>
+
+                
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input id="password" type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                <div className="form-group">
+                    <label>Re-enter Password</label>
+                    <input id="confirmpassword" type="password" className="form-control" placeholder="Confirm password" />
+                </div>
+                <br></br>
+                <button id="submitButton" type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <br></br>
+                <div class="text-center" >
+                <a href="/login" id="signinLink" >Already a member?Click here</a>
+            </div>
+            </form>
+            </div>
+        );
+    }
+}
